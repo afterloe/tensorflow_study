0779a7dcc888d944265d9017e4bce11876e1ca5b
liumin <liumin@ascs.tech>
2022-05-09 17:41:00 +0800

    feat: 手写体数据下载

---
+++ 03_lenet/le_network.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers.convolutional import Conv2D, MaxPooling2D
+from tensorflow.python.keras.layers.core import Activation, Flatten, Dense
+from tensorflow.python.keras import backend as T
+
+
+class LeNet:
+
+    @staticmethod
+    def build(width: int, height: int, depth: int, classes: int):
+        model = Sequential()
+        inputShape = (width, height, depth)
+        if T.image_data_format() == "channels_first":
+            inputShape = (depth, width, height)
+        model.add(Conv2D(20, (5, 5), padding="same", input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Conv2D(20, (5, 5), padding="same"))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(500))
+        model.add(Activation("relu"))
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        return model
+
+
+def show_in_plt(H, epochs: int = 100):
+    import matplotlib.pyplot as plt
+    from numpy import arange
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(arange(0, epochs), H.history["loss"], label="train_loss")
+    plt.plot(arange(0, epochs), H.history["val_loss"], label="val_loss")
+    plt.plot(arange(0, epochs), H.history["accuracy"], label="accuracy")
+    plt.plot(arange(0, epochs), H.history["val_accuracy"], label="val_accuracy")
+    plt.title("Training Loss and Accuracy")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend()
+    plt.show()
---
+++ 03_lenet/ln_mnist.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+from tensorflow.python.keras.optimizers import gradient_descent_v2
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn import datasets
+from tensorflow.python.keras import backend as T
+
+from le_network import LeNet, show_in_plt
+
+
+def main():
+    print("[info] accessing MNIST ...")
+    dataset = datasets.fetch_openml("mnist_784")
+    data = dataset.data
+    if T.image_data_format() == "channels.first":
+        data = data.values.reshape(data.shape[0], 1, 28, 28)
+    else:
+        data = data.values.reshape(data.shape[0], 28, 28, 1)
+    trainX, testX, trainY, testY = train_test_split(data / 255.0, dataset.target.astype("int"), test_size=0.25,
+                                                    random_state=42)
+    lb = LabelBinarizer()
+    trainY = lb.fit_transform(trainY)
+    testY = lb.fit_transform(testY)
+    print("[info] compiling model ...")
+    opt = gradient_descent_v2.SGD(learning_rate=0.01)
+    model = LeNet.build(28, 28, 1, 10)
+    model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+    H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=128, epochs=20, verbose=1)
+    print("[info] evaluating network ...")
+    predictions = model.predict(testX, batch_size=128)
+    print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1),
+                                target_names=[str(item) for item in lb.classes_]))
+    show_in_plt(H, 20)
+
+
+if "__main__" == __name__:
+    main()
