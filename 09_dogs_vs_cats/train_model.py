46e4f341286bb097c089c99b5109b8c586dd006d
liumin <liumin@ascs.tech>
2022-05-13 13:56:36 +0800

    feat: 登录调整信息

--- 09_dogs_vs_cats/train_model.py
+++ 09_dogs_vs_cats/train_model.py
@@ -12,14 +12,17 @@ def main():
     dataset = File(args["dataset"], "r")
     idx = int(dataset["labels"].shape[0] * 0.75)
     print("[info] tuning hyper parameters")
-    params = {"C": [0.0001, 0.001, 0.01, 0.1, 1.0]}
-    model = GridSearchCV(LogisticRegression(), params, cv=3, n_jobs=args["jobs"])
-    model.fit(dataset["feature"][:idx], dataset["labels"][:idx])
+    params = {"C": [0.001, 0.01, 0.1, 1.0]}
+    model = GridSearchCV(LogisticRegression(), params,
+                         cv=3, n_jobs=args["jobs"])
+    print("[info] fit model")
+    model.fit(dataset["features"][:idx], dataset["labels"][:idx])
     print("[info] best hyper parameter is %f", model.best_params_)
 
     print("[info] evaluating")
     predicts = model.predict(dataset["feature"][idx:])
-    print(classification_report(dataset["labels"][idx:], predicts, target_names=dataset["label_names"]))
+    print(classification_report(
+        dataset["labels"][idx:], predicts, target_names=dataset["label_names"]))
     acc = accuracy_score(dataset["labels"][idx:], predicts)
     print("[info] score: %f%%" % (acc * 100))
 
@@ -32,10 +35,15 @@ def main():
 
 
 if "__main__" == __name__:
+    r"""
+        python train_model.py -d D:\Datasets\output\kaggle_dogs_vs_cats\hdf5\features.hdf5 -m D:\Datasets\output\kaggle_dogs_vs_cats\dogs_vs_cats.pickle
+    """
     from argparse import ArgumentParser
     ap = ArgumentParser()
     ap.add_argument("-d", "--dataset", required=True, help="path HDF5 dataset")
-    ap.add_argument("-m", "--model", required=True, help="path to output model")
-    ap.add_argument("-j", "--jobs", type=int, default=-1, help="# of jobs to run when tuning hyper parameters")
+    ap.add_argument("-m", "--model", required=True,
+                    help="path to output model")
+    ap.add_argument("-j", "--jobs", type=int, default=-1,
+                    help="# of jobs to run when tuning hyper parameters")
     args = vars(ap.parse_args())
     main()
